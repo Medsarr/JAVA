eddd2344951756597931c35fb389d105c514400e
Medsarr <68923180+Medsarr@users.noreply.github.com>
2020-07-29 19:59:30 +0300

    Add files via upload

---
+++ commentaire/commentaire.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+	<head>
+		<title> Commentaire</title>
+		<meta charset="utf-8">
+		<?php ?>
+
+	</head>
+	 <body>
+		<?php ?>
+		<form method="post" action="target.php">
+ 	<h1>Ajouter un commentaire</h1>
+	<p>
+		<label for="Votre nom"> Prenom</label>
+    	<input type="text" name="Votre nom" id="Votre nom">
+	</p>
+	<p>
+		<label for="Email">Email</label>
+    	<input type="password" name="Email" id="Email">
+	</p>
+
+	<p>
+		<label for="Page d'acceuil">Page d'acceuil</label>
+    	<input type="text" name="Page d'acceuil" id="Page d'acceuil">
+	</p>
+
+	<p>
+		<label for="Commentaire">Commentaire</label>
+    	<input type="text" name="Commentaire" id="Commentaire">
+	</p>
+
+
+	<p>
+		<input type="submit" value="Enregistrer">
+		<input type="submit" value="Aperçu">
+	</p>
+ 
+</form>
+	</body>
+
+
+</html>
---
+++ commentaire/target.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<p> Bienvenu</p>
+ 	<?php 
+ 		echo $_post['prenom'];
+ 	?>
+ 	<p>
+ 		voiçi votre Email
+ 		<?php 
+ 		echo $_Post['Email'];
+ 	?>
+ 	</p>
+ 	<p>
+ 		voiçi votre Email
+ 		<?php 
+ 		echo $_Post['Email'];
+ 	?>
+ 	</p>
+</body>
+</html>
